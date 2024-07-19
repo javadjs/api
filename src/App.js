4b003aaaf0d0a70ccb8bac0d7e25e3640182b8b0
javadpr <jjreactjs@gmail.com>
2024-07-19 17:13:31 +0430

    30_react-router-_-part1

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Sidebar from "./Sidebar";
 import Content from "./Content";
 import style from "./style.module.css";
 import { MainContext } from "./contexts/MainContext";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   const [showmenu, setshowmenu] = useState(false);
 
   return (
     <div>
+      <BrowserRouter>
       <MainContext.Provider value={{showmenu, setshowmenu}}>
         <Sidebar />
         <Content />
         <h2>javad</h2>
       </MainContext.Provider>
+      </BrowserRouter>
     </div>
   );
 };
